f7d927ce4bd5ec67cedee461e0187d64fb46e5cb
Subhakanta Sahoo <subhakantasahoo1122@gmail.com>
2026-08-09 17:28:15 +0530

    config env integrated

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,5 @@ RUN npm install --legacy-peer-deps --ignore-scripts
 
 COPY . .
 ENV PORT=3000
-ENV MONGO_URI=mongodb://mongo:27017/mydatabase
 EXPOSE 3000
 CMD ["node", "index.js"]
--- config/connect.js
+++ config/connect.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 async function myTask() {
     mongoose.set('strictQuery', false);
-    const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mydatabase';
+    const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}`;
+    const mongoUri = MONGO_URI;
 
     let attempts = 0;
 
